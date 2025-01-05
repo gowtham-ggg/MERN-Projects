ab6e345fc369f29bb3a9c579313c65ac128e999a
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-01-05 05:55:57 +0530

    added front-end for doctor appointment booking app (complete) started backend

--- doctor_appointment/frontend/src/components/NavBar.jsx
+++ doctor_appointment/frontend/src/components/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
 
       {token ? (
         <div className="flex items-center gap-1 cursor-pointer group relative">
-          <img src={assets.profile_pic} alt="profile" className="w-8 rounded-full" />
+          <img src={assets.profile_pic} alt="profile" className="w-8 rounded-full " />
           <img src={assets.dropdown_icon} alt="dropdown" className="w-2.5" />
           <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
             <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
@@ -70,6 +70,23 @@ const NavBar = () => {
           </button>
         </div>
       )}
+      <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="menu" />
+
+      {/* mobile menu  */}
+      
+      <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'}   md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+        <div className='flex items-center justify-between px-5 py-6'>
+          <img className='w-36' src={assets.logo} alt="logo" />
+          <img  className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="cross" />
+        </div>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+          <NavLink  onClick={()=>setShowMenu(false)} to="/"><p className={'px-4 py-2 rounded inline-block'}>HOME</p></NavLink>
+          <NavLink  onClick={()=>setShowMenu(false)} to="/doctors"><p className={'px-4 py-2 rounded inline-block'}>ALL DOCTORS</p></NavLink>
+          <NavLink onClick={()=>setShowMenu(false)} to="/about"><p className={'px-4 py-2 rounded inline-block'}>ABOUT</p></NavLink>
+          <NavLink  onClick={()=>setShowMenu(false)} to="/contact"><p className={'px-4 py-2 rounded inline-block'}>CONTACT</p></NavLink>
+        </ul>
+
+      </div>
     </div>
   );
 };
