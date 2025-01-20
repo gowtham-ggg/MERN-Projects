420d447a9b8ff53ec769985f6591c6aef647a765
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-01-20 13:14:57 +0530

    frontend update (30%)

---
+++ mern-auth/client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+      <img className='w-36 h-36 rounded-full mb-6' src={assets.header_img} alt="header" />
+      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey Developer
+         <img src={assets.hand_wave} alt="wave" className='w-8 aspect-square' /></h1>
+
+      <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
+      <p className='mb-8 max-w-md'>Welcome to our secure and user-friendly authentication platform! Whether you're signing up for the first time or logging in, we ensure a seamless and safe experience. Your privacy and security are our top priorities, powered by robust encryption and cutting-edge technology. Get started today and access your personalized features with ease!</p>
+        <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+    </div>
+  )
+}
+
+export default Header
--- mern-auth/client/src/pages/Home.jsx
+++ mern-auth/client/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <div
+  style={{
+    backgroundImage: `url('/bg_img.png')`,
+  }}
+  className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center"
+>
+  <NavBar />
+  <Header />
+</div>
+
   )
 }
 
