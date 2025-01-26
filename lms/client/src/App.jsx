c59c0c85ff9c32a3385928935c9e10a51d685e5b
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-01-26 19:59:35 +0530

    added new project

---
+++ lms/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className='text-center text-3xl font-extrabold'>
+      hello
+    </div>
+  )
+}
+
+export default App
---
+++ lms/client/src/components/student/TestimonialSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonialSection = () => {
+  return (
+    <div>
+      Testimonial
+    </div>
+  )
+}
+
+export default TestimonialSection
