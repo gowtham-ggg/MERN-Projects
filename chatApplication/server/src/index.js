197ce66e2ffd9cad6cfe328f30eca3752ce42b35
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-03-26 12:55:37 +0530

    added new project

--- chatApplication/server/src/index.js
+++ chatApplication/server/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -17,6 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
