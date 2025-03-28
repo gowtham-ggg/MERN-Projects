2233672eec4874367be2b727ec4910b62b3616e7
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-03-28 16:29:39 +0530

    connectify frontend (signup login completed) [updated 40%]

---
+++ chatApplication/client/src/store/useAuthStore.js
@@ -0,0 +1,73 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,  
+    isLoggingIn: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser: res.data });
+        } catch (error) {
+            set({ authUser: null });
+            console.error("Auth check failed:", error.response?.data || error.message);
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+    signup : async (data) =>{
+        set({isSigningUp : true});
+        try {
+            const res = await axiosInstance.post("auth/signup", data)
+            toast.success("Account created successfully")
+            set({authUser : res.data})
+        } catch (error) {
+            toast.error(error.response.data.message)
+            
+        }finally{
+            set({isSigningUp : false})
+        }
+    },
+
+    logout : async () =>{
+        try {
+            await axiosInstance.post("/auth/logout")
+            set({authUser : null})
+            toast.success("Logged out successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    },
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/auth/login", data);
+          set({ authUser: res.data });
+          toast.success("Logged in successfully");
+    
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isLoggingIn: false });
+        }
+      },
+      updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+          const res = await axiosInstance.put("/auth/update-profile", data);
+          set({ authUser: res.data });
+          toast.success("Profile updated successfully");
+        } catch (error) {
+          console.log("error in update profile:", error);
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isUpdatingProfile: false });
+        }
+      },
+}));
+
--- chatApplication/server/src/index.js
+++ chatApplication/server/src/index.js
@@ -14,7 +14,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials : true
+}));
 app.use(express.json())
 app.use(cookieParser())
 
