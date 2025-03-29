17ae4898c43a9cb3ac81ddb71b27cfc0f851fb55
Gowtham G <122986426+gowtham-ggg@users.noreply.github.com>
2025-03-29 18:23:54 +0530

    Delete chatApplication directory

--- chatApplication/frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" 
-    ? "http://localhost:7001/api" 
-    : "https://chat-app-sr0r.onrender.com/api",
-  withCredentials: true,
-});
