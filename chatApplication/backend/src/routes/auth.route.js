51b1c9f75968c2452e0ffcbdcda5dde01e60f148
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-03-29 18:02:41 +0530

    final fix-2

--- chatApplication/backend/src/routes/auth.route.js
+++ chatApplication/backend/src/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import upload from "../middleware/upload.middleware.js"; // Import multer middleware
 
 const router = express.Router();
 
@@ -9,8 +8,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// Use Multer for profile picture upload
-router.put("/update-profile", protectRoute, upload.single("profilePic"), updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
 
